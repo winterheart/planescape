1c029c49882a850ac76f062ec1350a432519047c
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2014-10-11 22:27:47 +0600

    Скрипт для упаковки инсталлятора

---
+++ planescape-packer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ITP='po/script/Infinity Text Parser/InfinityPacker.pl'
+IN_TRA="po/trans/0-106496.tra"
+OUT_TRA="installer/pst-l10n/tra/russian/0-106496.tra"
+PO_FILES="po/ru/LC_MESSAGES/"
+VERSION="1.00-rc3"
+
+echo "==== Making 0-106496.tra ===="
+"${ITP}" -t ${IN_TRA} -i ${PO_FILES} -o ${OUT_TRA}
+
+echo "==== Recoding 0-106496.tra ===="
+recode utf8..windows-1251 ${OUT_TRA}
+
+echo "==== Compressing archive ===="
+cd installer
+zip -9 -r ~/pst-l10n-${VERSION}.zip .
